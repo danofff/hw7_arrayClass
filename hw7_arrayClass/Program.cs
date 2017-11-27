eb886c034dd692484c5c0eea44ff2e086cb28e73
dan <danofff@mail.ru>
2017-11-27 13:21:44 +0600

    Добавьте файлы проекта.

---
+++ hw7_arrayClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw7_arrayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            arClass a1 = new arClass(10);
+            arClass a2 = new arClass(10);
+            a1.printArr();
+            a2.printArr();
+            Console.WriteLine("overload >");
+            Console.WriteLine(a1>a2);
+            Console.WriteLine("overload <");
+            Console.WriteLine(a1<a2);
+        }
+    }
+}
---
+++ hw7_arrayClass/arClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw7_arrayClass
+{
+    class arClass
+    {
+        private static Random rnd = new Random();
+        private int [] myArr;
+        public int [] MyArr
+        {
+            get { return myArr; }
+            set { myArr = value; }
+        }
+        private int arrSum;
+        private int ArrSum
+        {
+            get { return arrSum; }
+            set { arrSum = value; }
+        }
+
+        public arClass()
+        {
+            this.ArrSum = 0;
+            int size = rnd.Next(1, 10);
+            this.MyArr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                this.MyArr[i] = rnd.Next(1, 101);
+                this.ArrSum += this.MyArr[i];
+            }
+        }
+        public arClass(int size)
+        {
+            this.ArrSum = 0;
+            this.MyArr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                MyArr[i] = rnd.Next(1, 101);
+                this.ArrSum += this.MyArr[i];
+            }
+        }
+        public void printArr()
+        {
+
+            Console.Write("{ ");
+            for (int i = 0; i < MyArr.Length; i++)
+            {
+                Console.Write($"{this.MyArr[i]}, ");
+            }
+            Console.WriteLine("}");
+            Console.WriteLine($"SUM: {this.ArrSum}");
+        }
+        public static bool operator>(arClass obj1,arClass obj2)
+        {
+            return obj1.ArrSum > obj2.ArrSum;
+        }
+        public static bool operator <(arClass obj1, arClass obj2)
+        {
+            return obj1.ArrSum < obj2.ArrSum;
+        }
+    }
+}
